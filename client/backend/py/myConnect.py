35db283c2e62f52e8cfe935347696fb2d09af7ed
achao <zhangshichao628@sina.com>
2017-05-18 21:55:44 +0800

    pyspider重构爬虫

--- client/backend/py/myConnect.py
+++ client/backend/py/myConnect.py
@@ -1,6 +1,6 @@
 import  pymysql
 def myConnect():
-    conn = pymysql.connect(host='120.77.34.254', port=3306, user='root', passwd='***', db='news', charset="utf8")
+    conn = pymysql.connect(host='120.77.34.254', port=3306, user='root', passwd='exciting', db='news', charset="utf8")
     return conn
 def loseConnect(conn,cursor):
     conn.commit()
