b475d4cfc22d068f75f635b46500772041aafd1a
achao <zhangshichao628@sina.com>
2017-05-27 19:41:36 +0800

    用户名和密码检查

--- client/frontend/src/js/index.js
+++ client/frontend/src/js/index.js
@@ -1,15 +1,27 @@
 $(document).ready(function(){
-    $("#sendCode").click(function(){
-        var email=$("#signUp .modal-body input").val();
+    $('#user_name').bind('blur',function(){ // 检查账号合法性
+        var user_name=$('#user_name').val();
+        if(user_name.length<6){
+             $('.warn-info').first().text('* 用户名至少6位字符');
+             return;
+        }
         $.ajax({
-            url:"client/backend/php/sendCode.php?email="+email,
+            url:"client/backend/php/checkUserName.php?user_name="+user_name,
             method:"get",
             success:function(data){
-                if(data.code==1){
-                    console.log("无重复");
+                if(data.num>0){
+                    $('.warn-info').first().text('* 用户名重复');
+                }else{
+                    $('.warn-info').first().text('');
                 }
             }
         });
+    });
+    $("#signUp input:eq(1)").bind('blur',function(){
+        var password=$(this).val();
+        if(password.length<6){
+            $('.warn-info:eq(1)').text('* 密码至少6位字符');
+        }
     });
 });
 
