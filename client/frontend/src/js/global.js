091fc79f6563460468c35f96b68b5c15ee3323cb
achao <zhangshichao628@sina.com>
2017-05-27 23:39:14 +0800

    密码一致性和邮箱合法性验证

--- client/frontend/src/js/global.js
+++ client/frontend/src/js/global.js
@@ -143,6 +143,26 @@ $(document).ready(function(){
         var password=$(this).val();
         if(password.length<6){
             $('.warn-info:eq(1)').text('* 密码至少6位字符');
+        }else{
+            $('.warn-info:eq(1)').text('');
+        }
+    });
+    $('#signUp input:eq(2)').bind('blur',function(){
+        var rePassword=$(this).val();
+        var password=$('#signUp input:eq(1)').val();
+        if(password!=rePassword){
+            $('.warn-info:eq(2)').text('* 密码不一致');
+        }else{
+            $('.warn-info:eq(2)').text('');
+        }
+    });
+    $('#signUp input:eq(3)').bind('blur',function(){
+        var email=$(this).val();
+        var reg=/^\w+@\w+\.(com)$/;
+        if(!email.match(reg)){
+            $('.warn-info:eq(3)').text('* 不是有效邮箱');
+        }else{
+            $('.warn-info:eq(3)').text('');
         }
     });
 });
